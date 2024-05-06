76b98a219ed98f311d52a6efa8b8024cdaca4828
Aakash Verma <dev.aakashv@gmail.com>
2024-05-06 21:00:11 +0530

    `add`: the styles, fonts into the `tailwind` configuration file to make it available in every component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "element_dark_blue": "hsl(209, 23%, 22%)",
+        "bgdark_very_dark_blue": "hsl(207, 26%, 17%)",
+        "text_light_very_dark_blue": "hsl(200, 15%, 8%)",
+        "input_light_dark_gray": "hsl(0, 0%, 52%)",
+        "bglight_very_light_gray": "hsl(0, 0%, 98%)",
+      },
+      fontWeight: {
+        "regular": 300,
+        "semibold": 600,
+        "extrabold": 800
+      },
+      fontFamily: {
+        'nunito': ["Nunito Sans", "serif"]
+      }
+    },
   },
   plugins: [],
 }
