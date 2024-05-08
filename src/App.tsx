b45d64b46b51ff1df789cda1b7007274843751e4
Aakash Verma <dev.aakashv@gmail.com>
2024-05-08 23:01:15 +0530

    `add`: move the header content into a separate component and use the tailwind class

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Layout } from 'antd';
+import './App.css';
+import HeaderContent from './components/HeaderContent/HeaderContent';
+
+const { Header, Footer, Content } = Layout;
+
 
 const App = () => {
+  
   return (
-    <div>App</div>
+    <>
+      <Header className='bg-transparent !py-0 p-2 !m-0'>
+        <HeaderContent />
+      </Header>
+        <Content>
+
+        </Content>
+      <Footer></Footer>
+    </>
   )
 }
 
