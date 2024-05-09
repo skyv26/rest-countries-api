e9b9403837f5fd4d963084567cdc91de86330502
Aakash Verma <dev.aakashv@gmail.com>
2024-05-10 01:56:26 +0530

    `add`: the browser router into the main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { CountryProvider } from "./context/CountryContext.tsx";
+import "./index.css";
+
+import { BrowserRouter, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CountryProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <App />
+        </Routes>
+      </BrowserRouter>
     </CountryProvider>
   </StrictMode>
 );
