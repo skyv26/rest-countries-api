2d945e8349b49b0a7cf0ece1e6410835e5f2c165
Aakash Verma <dev.aakashv@gmail.com>
2024-05-10 01:53:47 +0530

    `add`: pages component with placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { Layout } from 'antd';
-import './App.css';
-import HeaderContent from './components/HeaderContent/HeaderContent';
-import FilterContainer from './components/FilterContainer/FilterContainer';
+import { Card, Layout } from "antd";
+import "./App.css";
+import FilterContainer from "./components/FilterContainer/FilterContainer";
+import HeaderContent from "./components/HeaderContent/HeaderContent";
 
 const { Header, Footer, Content } = Layout;
+const { Meta } = Card;
 
 const App = () => {
-  
   return (
     <>
-      <Header className='bg-transparent !py-0 p-2 !m-0'>
+      <Header className="bg-transparent !py-0 p-2 !m-0">
         <HeaderContent />
       </Header>
-        <Content>
-            <FilterContainer />
-        </Content>
+      <Content>
+        <FilterContainer />
+        <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
+      </Content>
       <Footer></Footer>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
