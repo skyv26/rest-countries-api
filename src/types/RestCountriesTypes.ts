1751b2fb48e0d88e92b398828fb6b418d34b69ed
Aakash Verma <dev.aakashv@gmail.com>
2024-05-09 23:31:10 +0530

    `add`: the types or interfaces in order to make sure the type of the data

---
+++ src/types/RestCountriesTypes.ts
@@ -0,0 +1,63 @@
+export interface Currency {
+    code: string;
+    name: string;
+    symbol: string;
+  }
+  
+  export interface Language {
+    iso639_1: string;
+    iso639_2: string;
+    name: string;
+    nativeName: string;
+  }
+  
+  export interface Translation {
+    br: string;
+    pt: string;
+    nl: string;
+    hr: string;
+    fa: string;
+    de: string;
+    es: string;
+    fr: string;
+    ja: string;
+    it: string;
+    hu: string;
+  }
+  
+  export interface RegionalBloc {
+    acronym: string;
+    name: string;
+  }
+  
+  export interface Country {
+    name: string;
+    topLevelDomain: string[];
+    alpha2Code: string;
+    alpha3Code: string;
+    callingCodes: string[];
+    capital: string;
+    altSpellings: string[];
+    subregion: string;
+    region: string;
+    population: number;
+    latlng: [number, number];
+    demonym: string;
+    area: number;
+    timezones: string[];
+    borders: string[];
+    nativeName: string;
+    numericCode: string;
+    flags: {
+      svg: string;
+      png: string;
+    };
+    currencies: Currency[];
+    languages: Language[];
+    translations: Translation;
+    flag: string;
+    regionalBlocs: RegionalBloc[];
+    cioc: string;
+    independent: boolean;
+  }
+  
