210a2b59305cdbb31234bf8321775aa8d7b44350
Aakash Verma <dev.aakashv@gmail.com>
2024-05-12 12:46:41 +0530

    `add`: the tailwind classes to style or manage the layout

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,8 +3,8 @@ import { Input } from "antd"
 
 const Search = () => {
   return (
-    <div>
-        <Input placeholder="default size" prefix={<SearchOutlined />} />
+    <div className="bg-white shadow-sm ">
+        <Input className="bg-transparent !py-4 !px-5 border-none flex gap-2 !text-[18px] placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
     </div>
   )
 }
