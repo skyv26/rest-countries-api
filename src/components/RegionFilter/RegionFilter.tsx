d26e34493167156639d34d214e38e803a59dcede
Aakash Verma <dev.aakashv@gmail.com>
2024-05-12 12:46:37 +0530

    `add`: the tailwind classes to style or manage the layout

--- src/components/RegionFilter/RegionFilter.tsx
+++ src/components/RegionFilter/RegionFilter.tsx
@@ -1,37 +1,54 @@
-import { DownOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Dropdown, Space, Typography } from 'antd';
+import { DownOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Dropdown, Space, Typography } from "antd";
+import { useCountryContext } from "../../context/CountryContext";
 
-const items: MenuProps['items'] = [
-  {
-    key: '1',
-    label: 'Item 1',
-  },
-  {
-    key: '2',
-    label: 'Item 2',
-  },
-  {
-    key: '3',
-    label: 'Item 3',
-  },
-];
+// const items: MenuProps["items"] = [
+//   {
+//     key: "1",
+//     label: "Item 1",
+//   },
+//   {
+//     key: "2",
+//     label: "Item 2",
+//   },
+//   {
+//     key: "3",
+//     label: "Item 3",
+//   },
+// ];
 
 const RegionFilter = () => {
-  return <Dropdown
-  menu={{
-    items,
-    selectable: true,
-    defaultSelectedKeys: ['3'],
-  }}
->
-  <Typography.Link>
-    <Space>
-      Selectable
-      <DownOutlined />
-    </Space>
-  </Typography.Link>
-</Dropdown>;
+  const { countries, loading, error } = useCountryContext();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const uniqueRegions = [...new Set(countries.map((country) => country.region))].sort((a, b) => a.localeCompare(b));
+
+  const items: MenuProps["items"] = uniqueRegions.map((each_region) => ({
+    key: each_region.toLowerCase(),
+    label: each_region
+  }))
+
+  return (
+    <Dropdown
+      className="!bg-white !text-text_light_very_dark_blue !py-4 !px-5"
+      menu={{
+        items,
+        className: "text-text_light_very_dark_blue",
+        selectable: true,
+        defaultSelectedKeys: ["3"],
+      }}
+    >
+      <Typography.Link>
+        <Space>
+          Filter by Region
+          <DownOutlined />
+        </Space>
+      </Typography.Link>
+    </Dropdown>
+  );
 };
 
 export default RegionFilter;
