3f5c729deaf04f7f749ba82595a5a4d4a6ac2ed4
Aakash Verma <dev.aakashv@gmail.com>
2024-05-13 13:47:34 +0530

    `update`: the style for the `filter` container using tailwindcss classes

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,7 +4,7 @@ import { Input } from "antd"
 const Search = () => {
   return (
     <div className="bg-white shadow-sm ">
-        <Input className="bg-transparent !py-4 !px-5 border-none flex gap-2 !text-[18px] placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
+        <Input className="bg-transparent !py-3 !px-5 border-none flex gap-2 !text-base placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
     </div>
   )
 }
