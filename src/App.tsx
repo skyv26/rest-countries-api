c6807182b0cf16eead373129b2e562c7bb06cd38
Aakash Verma <dev.aakashv@gmail.com>
2024-05-14 14:25:52 +0530

    `update`: the style for the flag card and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Card, Col, Layout, Row } from "antd";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import FilterContainer from "./components/FilterContainer/FilterContainer";
 import HeaderContent from "./components/HeaderContent/HeaderContent";
-import { Route, Routes } from "react-router";
 import Detail from "./pages/Detail";
 
 const { Header, Footer, Content } = Layout;
@@ -11,7 +11,7 @@ const { Meta } = Card;
 const App = () => {
   return (
     <>
-      <Header className="!py-0 p-2 !m-0 !bg-white shadow-sm">
+      <Header className="!p-0 h-auto !m-0 !bg-white shadow-sm">
         <HeaderContent />
       </Header>
       <Content className="content bg-[#bbb8b8] !p-2 !py-6">
@@ -25,16 +25,22 @@ const App = () => {
                 path="/"
                 element={
                   <Card
-                  className="!rounded-[5px]"
+                  className="!rounded-[5px] max-w-[264px]"
                     cover={
                       <img
                         alt="example"
-                        className="w-[264px] h-[160px] object-fill object-top"
+                        className="w-full h-[160px] object-fill object-top rounded-none"
                         src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
                       />
                     }
                   >
-                    {/* Additional Card Content */}
+                    <Meta title="Germany" description={
+                      <ul>
+                        <li>Population: <span>81770900</span></li>
+                        <li>Region: <span>Europe</span></li>
+                        <li>Capital: <span>Berlin</span></li>
+                      </ul>
+                    } />
                   </Card>
                 }
               />
