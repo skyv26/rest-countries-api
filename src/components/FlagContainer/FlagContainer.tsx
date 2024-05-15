38a1d0135774338e42f00dbad4c2cbc3d0e99b9b
Aakash Verma <dev.aakashv@gmail.com>
2024-05-15 16:59:53 +0530

    `add`: the new component to hold the flag as container

---
+++ src/components/FlagContainer/FlagContainer.tsx
@@ -0,0 +1,32 @@
+import { Col, Row } from "antd";
+import FlagCard from "../FlagCard/FlagCard";
+import { Country } from "../../types/RestCountriesTypes";
+import { useCountryContext } from "../../context/CountryContext";
+
+const FlagContainer = () => {
+  const { countries, loading, error } = useCountryContext();
+  const defaultCountriesAsPerDesign =
+    "india,united states of america,pakistan,Russian Federation,United Kingdom of Great Britain and Northern Ireland,japan,Sierra Leone";
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const predefinedCountries = countries.filter((country: Country) =>
+    defaultCountriesAsPerDesign.toLowerCase().includes(country.name.toLowerCase())
+  );
+
+  return (
+    <Row className="w-full py-8 gap-10" justify="center">
+        {
+            predefinedCountries.map((each: Country) => (
+                <Col>
+        <FlagCard country={each} />
+      </Col>
+            ))
+        }
+      
+    </Row>
+  );
+};
+
+export default FlagContainer;
