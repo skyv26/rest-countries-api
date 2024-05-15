353cb59959b5454e1419ab8009898303ceb5a71a
Aakash Verma <dev.aakashv@gmail.com>
2024-05-15 17:01:36 +0530

    `refactor`: the card component style and add the content

--- src/components/FlagCard/FlagCard.tsx
+++ src/components/FlagCard/FlagCard.tsx
@@ -1,8 +1,48 @@
+import { Card, Image } from "antd";
+import { Country } from "../../types/RestCountriesTypes";
+const { Meta } = Card;
 
-const FlagCard = () => {
+const FlagCard = ({ country }: { country: Country }) => {
   return (
-    <div>FlagCard</div>
-  )
-}
+    <Card
+      className="!rounded-[5px] w-52 max-w-[264px] overflow-hidden border-none"
+      // className="w-full h-[160px] rounded-none"
 
-export default FlagCard
+      cover={
+        <>
+          <div
+            className="w-full h-[160px] rounded-none bg-no-repeat bg-center"
+            style={{ backgroundImage: `url(${country.flag})`, backgroundSize: "auto 100%", backgroundPosition: "center" }}
+            
+          />
+          {/* <div className="w-full h-[160px]">
+            <Image
+              alt={country.altSpellings.join(",")}
+              src={country.flag}
+              className="!h-[160px] !inline-block"
+            />
+          </div> */}
+        </>
+      }
+    >
+      <Meta
+        title={country.name}
+        description={
+          <ul>
+            <li className="max-w-[200px] truncate">
+              Population: <span>{country.population}</span>
+            </li>
+            <li className="max-w-[200px] truncate">
+              Region: <span>{country.region}</span>
+            </li>
+            <li className="max-w-[200px] truncate">
+              Capital: <span>{country.capital}</span>
+            </li>
+          </ul>
+        }
+      />
+    </Card>
+  );
+};
+
+export default FlagCard;
