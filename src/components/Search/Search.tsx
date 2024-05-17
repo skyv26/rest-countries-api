444c094a64774adae18836e5f3f20b9092b7ba39
Aakash Verma <dev.aakashv@gmail.com>
2024-05-17 18:10:25 +0530

    `refactor`: the component by adding the search filter set hook

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,15 @@
 import { SearchOutlined } from "@ant-design/icons"
 import { Input } from "antd"
+import { useCountryContext } from "../../context/CountryContext";
 
 const Search = () => {
+  const { setSearchTerm } = useCountryContext();
+
   return (
     <div className="bg-white shadow-sm ">
-        <Input className="bg-transparent !py-3 !px-5 border-none flex gap-2 !text-base placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
+        <Input onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setSearchTerm(e.target.value)
+        }} className="bg-transparent !py-3 !px-5 border-none flex gap-2 !text-base placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
     </div>
   )
 }
