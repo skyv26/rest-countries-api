beb2ceef83a4b82ab09fdd71135660703e889189
Aakash Verma <dev.aakashv@gmail.com>
2024-05-18 19:18:29 +0530

    `refactor`: the component by adding the filtered countries data

--- src/components/FlagContainer/FlagContainer.tsx
+++ src/components/FlagContainer/FlagContainer.tsx
@@ -4,24 +4,24 @@ import { Country } from "../../types/RestCountriesTypes";
 import { useCountryContext } from "../../context/CountryContext";
 
 const FlagContainer = () => {
-  const { countries, loading, error } = useCountryContext();
+  const { filteredCountries, loading, error } = useCountryContext();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
-  const defaultCountriesAsPerDesign =
-    "india,united states of america,pakistan,Russian Federation,United Kingdom of Great Britain and Northern Ireland,japan,Sierra Leone";
+//   const defaultCountriesAsPerDesign =
+//     "india,united states of america,pakistan,Russian Federation,United Kingdom of Great Britain and Northern Ireland,japan,Sierra Leone";
 
-  const predefinedCountries = countries.filter((country: Country) =>
-    defaultCountriesAsPerDesign
-      .toLowerCase()
-      .includes(country.name.toLowerCase())
-  );
+//   const predefinedCountries = countries.filter((country: Country) =>
+//     countries
+//       .toLowerCase()
+//       .includes(country.name.toLowerCase())
+//   );
 
   return (
     <Row className="w-full py-8 gap-10" justify="center">
-      {predefinedCountries.map((each: Country) => (
-        <Col>
+      {filteredCountries.map((each: Country) => (
+        <Col key={each.name}>
           <FlagCard country={each} />
         </Col>
       ))}
