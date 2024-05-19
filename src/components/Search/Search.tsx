d76753e14729cb2efa64e1a7210f297d1d29bc5b
Aakash Verma <dev.aakashv@gmail.com>
2024-05-19 19:19:31 +0530

    `refactor`: the component to ensure the functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,9 @@
 import { SearchOutlined } from "@ant-design/icons"
 import { Input } from "antd"
 import { useCountryContext } from "../../context/CountryContext";
+import { memo } from "react";
 
-const Search = () => {
+const Search = memo(() => {
   const { setSearchTerm } = useCountryContext();
 
   return (
@@ -12,6 +13,6 @@ const Search = () => {
         }} className="bg-transparent !py-3 !px-5 border-none flex gap-2 !text-base placeholder:!font-nunito !text-input_light_dark_gray" placeholder="Search for a country..." prefix={<SearchOutlined className="text-input_light_dark_gray" />} />
     </div>
   )
-}
+});
 
 export default Search
