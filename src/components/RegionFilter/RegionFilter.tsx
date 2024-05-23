24b99487e71ad97d9c88d3b3acbff3e2c9b124f7
Aakash Verma <dev.aakashv@gmail.com>
2024-05-24 01:28:03 +0530

    `refactor`: the component by matching the mobile design

--- src/components/RegionFilter/RegionFilter.tsx
+++ src/components/RegionFilter/RegionFilter.tsx
@@ -1,6 +1,6 @@
 import { DownOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Dropdown, Space, Typography } from "antd";
+import { Dropdown, Typography } from "antd";
 import { useCountryContext } from "../../context/CountryContext";
 
 const RegionFilter = () => {
@@ -10,53 +10,49 @@ const RegionFilter = () => {
   if (error) return <p>Error: {error}</p>;
 
   // Get unique regions sorted alphabetically
-  const uniqueRegions = [...new Set(countries.map((country) => country.region))].sort((a, b) =>
-    a.localeCompare(b)
-  );
+  const uniqueRegions = [
+    ...new Set(countries.map((country) => country.region)),
+  ].sort((a, b) => a.localeCompare(b));
 
   // Define items for the dropdown
   const items: MenuProps["items"] = [
     {
-      key: 'default',
-      label: 'Default'
+      key: "default",
+      label: "Default",
     },
     ...uniqueRegions.map((each_region) => ({
       key: each_region.toLowerCase(),
-      label: each_region
-    }))
+      label: each_region,
+    })),
   ];
 
   const handleMenuClick: MenuProps["onClick"] = (e) => {
     // Find the selected item based on key
     const selectedItem = items.find((item) => item?.key === e.key) as {
-      key: string,
-      label: string
+      key: string;
+      label: string;
     };
 
     if (selectedItem) {
       const selectedLabel = selectedItem.label;
-      console.log("Selected Region Key:", e.key);
-      console.log("Selected Region Label:", selectedLabel);
       setRegionFilter(selectedLabel);
     }
   };
 
   return (
     <Dropdown
-      className="!bg-white !text-text_light_very_dark_blue !py-[18px] !px-5"
+      className="!bg-white !text-text_light_very_dark_blue !py-[15px] !px-5 rounded-[5px] !shadow-sm"
       menu={{
         items,
         className: "text-text_light_very_dark_blue",
         selectable: true,
         defaultSelectedKeys: ["default"], // Set the default selected key
-        onClick: handleMenuClick
+        onClick: handleMenuClick,
       }}
     >
-      <Typography.Link>
-        <Space>
-          Filter by Region
-          <DownOutlined />
-        </Space>
+      <Typography.Link className="flex justify-between text-xs items-center w-full">
+        <span>Filter by Region</span>
+        <DownOutlined className="!text-[8px] items-end" />
       </Typography.Link>
     </Dropdown>
   );
