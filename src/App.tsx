32f15f94366501e649adb9d17aed1e9d89f9f1b3
Aakash Verma <dev.aakashv@gmail.com>
2024-05-25 02:35:08 +0530

    `fix`: the style as per the design

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,13 @@ import HeaderContent from "./components/HeaderContent/HeaderContent";
 import Detail from "./pages/Detail";
 
 const { Header, Footer, Content } = Layout;
-// bglight_very_light_gray
 const App = () => {
   return (
     <>
-      <Header className="!p-0 h-auto !m-0 !bg-white !shadow-md">
+      <Header className="!p-0 h-auto !m-0 !bg-transparent !shadow-md dark:!bg-element_dark_blue">
         <HeaderContent />
       </Header>
-      <Content className="content bg-bglight_very_light_gray  !p-2 !py-6 min-[375px]:!p-4">
+      <Content className="content bg-bglight_very_light_gray dark:bg-bgdark_very_dark_blue !p-2 !py-6 min-[375px]:!p-4">
         <Row gutter={[16, 16]}>
           <Col span={24}>
             <Routes>
