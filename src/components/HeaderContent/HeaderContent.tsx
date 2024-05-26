8cfd109d59535de0e5531f5557296a9922ce5187
Aakash Verma <dev.aakashv@gmail.com>
2024-05-27 03:01:13 +0530

    `fix`: responsive design for the tablet

--- src/components/HeaderContent/HeaderContent.tsx
+++ src/components/HeaderContent/HeaderContent.tsx
@@ -19,15 +19,15 @@ const HeaderContent = () => {
   
   return (
     <>
-      <Row justify="space-between" align="middle" className="!py-[10px] !px-4 min-[375px]:!py-[8px]">
+      <Row justify="space-between" align="middle" className="!py-[10px] !px-4 min-[375px]:!py-[8px] md:!px-20">
         <Col>
-          <Title className="heading-style font-extrabold !text-text_light_very_dark_blue !text-[13.5px] dark:!text-bglight_very_light_gray" level={1}>
+          <Title className="heading-style font-extrabold !text-text_light_very_dark_blue !text-[13.5px] dark:!text-bglight_very_light_gray md:!text-2xl" level={1}>
             Where in the world?
           </Title>
         </Col>
         <Col>
           <Button
-            className="text-xs !p-0 !m-0 !text-text_light_very_dark_blue !bg-transparent dark:!text-bglight_very_light_gray border-none font-semibold"
+            className="text-xs !p-0 !m-0 !text-text_light_very_dark_blue !bg-transparent dark:!text-bglight_very_light_gray border-none font-semibold md:!text-base"
             size="small"
             onClick={() => {
               setIsLightMode(!isLightMode);
@@ -38,7 +38,7 @@ const HeaderContent = () => {
               // } else {
               // }
             }}
-            icon={isLightMode ? <MoonFilled className="!text-text_light_very_dark_blue !text-sm dark:!text-bglight_very_light_gray" /> : <MoonOutlined className="!text-text_light_very_dark_blue !text-sm dark:!text-bglight_very_light_gray" />}
+            icon={isLightMode ? <MoonFilled className="!text-text_light_very_dark_blue !text-sm dark:!text-bglight_very_light_gray md:!text-base" /> : <MoonOutlined className="!text-text_light_very_dark_blue !text-sm dark:!text-bglight_very_light_gray" />}
           >
             {isLightMode ? "Light" : "Dark"} Mode
           </Button>
